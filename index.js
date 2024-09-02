61ca4890e8f5d07f762225e6b00960cc9f682943
Emily Orduz <62402866+Emilyuwu0@users.noreply.github.com>
2024-09-02 15:43:46 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+console.log("hola");
+
+/*
+ Definiciones:
+-------------------------------------------------------
+Tipado estatico 
+Tipado dinamico
+-------------------------------------------------------
+Let: let puede modificarse pero no volver a declararse.
+Const: const no puede modificarse ni volver a declararse 
+*/
